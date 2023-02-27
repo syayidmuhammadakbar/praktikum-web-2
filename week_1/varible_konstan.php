0105568f1d136726dc66c6cd44ad8fdc600fde25
syayid muhammad akbar <syayid.m.akbar@gmail.com>
2023-02-27 15:08:42 +0700

    pertemuan pertama

---
+++ week_1/varible_konstan.php
@@ -0,0 +1,19 @@
+<?php
+//variable konstan
+define('phi',3.14);
+
+//soal 1 hitunglah luas lingkaran dengan jari-jari 8
+// hitunglah keililing lingkaran degan jari jari 8
+
+// deiinisikan jari-jari
+$jari = 8;
+$luas = phi * $jari * $jari;
+$kll = 2 * phi * $jari;
+
+echo"luas lingkaran dengan jari-jari $jari adalah $luas";
+echo"<br/>keliling lingkaran dengan jari-jari $jari adalah $kll";
+
+//=============
+
+
+?>
